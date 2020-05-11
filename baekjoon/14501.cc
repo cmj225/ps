1768f580b8ecd047dff758ca294d7dd36f8d6404
Minjae Cheon <cheonminjae225@gmail.com>
2020-05-11 12:26:30 +0900

    source correction

--- baekjoon/14501.cc
+++ baekjoon/14501.cc
@@ -1,22 +1,20 @@
 // https://www.acmicpc.net/problem/14501
 
 #include <stdio.h>
-#include <limits>
 
 int N;// 1<=N<=15
 int array[15][2] = { 0 };
-int temp = 0;
-int ans = std::numeric_limits<int>::min();
+int temp = 0, ans = 0;
 
 void scheduleling(int day) {
 	int task_end = day + array[day][0] - 1;
-	if (task_end > N || day > N) {
+	if (task_end > N - 1 || day > N - 1) {
 		ans = ans > temp ? ans : temp;
 		return ;
 	}
 
 	temp += array[day][1];
-	for (int i = task_end; i <= N; i++) {
+	for (int i = task_end; i < N; i++) {
 		scheduleling(i+1);
 	}
 	temp -= array[day][1];
@@ -25,11 +23,12 @@ void scheduleling(int day) {
 int main() {
 	scanf("%d", &N);
 
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		scanf("%d %d", &array[i][0], &array[i][1]);
 	}
 
-	scheduleling(1);
+	for (int i = 0; i < N; i++)
+		scheduleling(i);
 
 	printf("%d\n", ans);
 
